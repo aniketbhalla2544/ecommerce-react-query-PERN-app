a9fe79a6eeab695d932044212beb9d3e3c4493ba
aniketbhalla2544 <aniketbhalla2544@gmail.com>
2023-11-06 13:34:03 -0400

    client: Added react query and npm packs

---
+++ client/src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from 'react-hot-toast';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import App from './App.tsx';
+import './index.css';
+
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <Toaster />
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>
+);

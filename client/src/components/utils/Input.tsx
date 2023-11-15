ccb663cf333796d6bfa6c39bd0cac67f4879e847
Aniket Bhalla <aniketbhalla2544@gmail.com>
2023-11-15 18:19:22 -0400

    client: created reusable styled input

---
+++ client/src/components/utils/Input.tsx
@@ -0,0 +1,42 @@
+import { DetailedHTMLProps, InputHTMLAttributes } from 'react';
+import RequiredInputSymbol from './RequiredInputSymbol';
+
+type ThisProps = DetailedHTMLProps<
+  InputHTMLAttributes<HTMLInputElement>,
+  HTMLInputElement
+> & {
+  labelText: string;
+  htmlFor: string;
+  isInputError: boolean;
+  errorMsg: string;
+};
+
+const Input = ({
+  labelText,
+  htmlFor,
+  isInputError,
+  errorMsg,
+  ...otherProps
+}: ThisProps) => {
+  const ringColorStyle = isInputError
+    ? 'border-0 focus:border-0 ring-2 ring-red-500'
+    : 'focus:ring-blue-500 focus:ring-offset-2';
+
+  return (
+    <div className='flex flex-col gap-y-3'>
+      <label htmlFor={htmlFor}>
+        {labelText.trim()}
+        <RequiredInputSymbol />
+      </label>
+      <input
+        className={`rounded-md border border-gray-300 px-4 py-3 text-sm focus:outline-none focus:ring-2  ${ringColorStyle}`}
+        {...otherProps}
+      />
+      {isInputError && errorMsg && <p className='text-red-500 text-xs'>{errorMsg}</p>}
+    </div>
+  );
+};
+
+export default Input;
+
+// className={`rounded-md border border-gray-300 px-4 py-3 text-sm focus:outline-none focus:ring-2 focus:ring-offset-2 ${ringColorStyle}`}

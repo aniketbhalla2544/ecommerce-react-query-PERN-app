79a79b6d18bb12ba9f72b468fd81401716c890e9
Aniket Bhalla <aniketbhalla2544@gmail.com>
2023-11-23 15:30:20 -0400

    client: updated Container.tsx
    styling

--- client/src/components/utils/Container.tsx
+++ client/src/components/utils/Container.tsx
@@ -1,10 +1,10 @@
 const Container = ({ children }: Record<'children', React.ReactNode>) => {
   return (
-    <div className='mx-auto px-3 sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'>
+    <div className='mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'>
       {children}
     </div>
   );
 };
 
 export default Container;
-// max-w-none md:max-w-md lg:max-w-5xl 2xl:max-w-screen-2xl
+// className='mx-auto sm:max-w-screen-sm md:max-w-screen-md lg:max-w-screen-lg xl:max-w-screen-xl'

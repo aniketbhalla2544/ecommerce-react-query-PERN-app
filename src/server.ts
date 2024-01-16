69418ebe12944e69f96bb61e7a72cf5e77eae241
aniketbhalla2544 <aniketbhalla2544@gmail.com>
2024-01-16 19:34:13 -0400

    app: updated package-lock.json

--- src/server.ts
+++ src/server.ts
@@ -32,7 +32,7 @@ cloudinary.config(appConfig.cloudinary);
 // routes
 app.get('/', async (req, res) => {
   return res.json({
-    msg: 'Vendor server running from docker jenkins',
+    msg: 'Vendor server running from ubuntu',
   });
 });
 
